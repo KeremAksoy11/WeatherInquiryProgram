c15ef8cb3237d4e80380065417acb29f6ca86189
Mehmet Kerem AKSOY <64080556+KeremAksoy11@users.noreply.github.com>
2021-04-08 16:46:37 +0300

    WeatherInquiryProgram
     Selenium ve C# programlama dilleri kullanılarak geliştirilmiştir. İstediğiniz şehrin ismini programa giriyorsunuz ve program çalışıyor. Program gerekli bilgileri toparladıktan sonra not defterine kayıt edip verileri getiriyor. It has been developed using Selenium and C # programming languages. You enter the name of the city you want in the program and the program runs. After the program collects the necessary information, it records it in the notebook and brings the data.

---
+++ SeleniumSecondProject/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Text;
+using wordeaktar = Microsoft.Office.Interop.Word;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Vbe.Interop;
+
+namespace SeleniumSecondProject
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            string sor, bugun;
+            Console.WriteLine("************* BU PROGRAM HAVA DURUMU SORGULAMA PROGRAMIDIR *************");
+            Console.Write("\nLufen Sehir Ismi Giriniz: ");
+            sor = Console.ReadLine();
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Task.Delay(2000);
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys(sor + " Hava Durumu");
+            element.Submit();
+            Task.Delay(1000);
+            Console.Write(sor + " Hava Durumu");
+            string santigrat = driver.FindElement(By.Id("wob_tm")).Text.Trim().ToString();
+            string state = driver.FindElement(By.Id("wob_dc")).Text.Trim().ToString();
+            string yagıs = driver.FindElement(By.Id("wob_pp")).Text.Trim().ToString();
+            string nem = driver.FindElement(By.Id("wob_hm")).Text.Trim().ToString();
+            string ruzgar = driver.FindElement(By.Id("wob_ws")).Text.Trim().ToString();
+            DateTime tarih = DateTime.Now;
+            bugun = (tarih.ToString("f"));
+            string bilgi = (bugun + "");
+            string satirlar;
+            satirlar  = ( "********************************\n" + "Gün: " + bilgi + "\nAranan Sehir: " + sor + "\nKaç Derece: " + santigrat + " Santigrat Derece" + "\nHava Durumu: " + state + "\nYağış Oranı: " + yagıs + "\nNem Oranı: " + nem + "\nRüzgar Şiddeti: " + ruzgar + "\n********************************" );
+            using (System.IO.StreamWriter dosya = new System.IO.StreamWriter(@"C:\Users\kerem\OneDrive\Belgeler\visual studio 2015\Projects\SeleniumSecondProject\SeleniumSecondProject\Gecmis.txt", true))
+            dosya.WriteLine(satirlar);
+            string dosya1 = @"C:\Users\kerem\OneDrive\Belgeler\visual studio 2015\Projects\SeleniumSecondProject\SeleniumSecondProject\Gecmis.txt";
+            driver.Quit();
+            Process.Start(@"C:\Users\kerem\OneDrive\Belgeler\visual studio 2015\Projects\SeleniumSecondProject\SeleniumSecondProject\Gecmis.txt");
+
+           
+        }
+       }
+ }
+
